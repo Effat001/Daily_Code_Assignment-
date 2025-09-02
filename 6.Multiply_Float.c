71854cd04415ea1ebfba1b95a7d222ea4c0bb7cd
Effat Ul Zannat Alif <132600538+Effat001@users.noreply.github.com>
2025-09-02 09:41:08 +0600

    Add files via upload
    Solved Problem 6

---
+++ 6.Multiply_Float.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float a,b,result;
+    printf("Enter two floating-Point Numbers: ");
+    scanf("%f %f",&a,&b);
+    result = a*b;
+    printf("\nThe Multiplication is: %.2f\n",result);
+
+
+  return 0;
+
+}
