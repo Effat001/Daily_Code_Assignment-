04ca3b4843061891b54de1fdccc5ea76a16f113a
Effat Ul Zannat Alif <132600538+Effat001@users.noreply.github.com>
2025-09-02 09:41:40 +0600

    Add files via upload
    Solved Problem 7

---
+++ 7.ASCII.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+ {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    printf("\nThe ASCII Value of %c is = %d\n", ch,ch );
+
+    return 0;
+ }
